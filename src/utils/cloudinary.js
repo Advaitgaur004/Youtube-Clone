9fe4417aa9e8115b144d8ed36bb8ff21163ca04f
Advait <126783821+Advaitgaur004@users.noreply.github.com>
2024-03-30 03:34:42 +0530

    Add access and refresh token secrets and expiration times

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadToCloudinary = async (file_path) => {
+    try {
+        if (!file_path) {
+            return;
+        }
+        const result = await cloudinary.uploader.upload(file_path,resource_type="auto");
+        console.log("file uploaded to cloudinary", result.secure_url);
+        return result;
+
+    } catch (error) {
+        fs.unlinkSync(file_path);
+        return null
+    }
+}
+
+export {uploadToCloudinary};
