5695ee9ecc673e0ffc158f1e34272bea6db584c8
Advait <126783821+Advaitgaur004@users.noreply.github.com>
2024-04-04 03:41:02 +0530

    Login logout and refresh endpoint is build

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,9 @@ import Apierror from '../utils/ApiErrors.js';
 import User from '../models/user.model.js';
 import {uploadToCloudinary} from '../utils/cloudinary.js';
 import APiResponse from '../utils/ApiResponse.js';
+import generateAccessAndRefreshToken from '../utils/generateTokens.js';
+import jwt from 'jsonwebtoken';
+
 const registerUser = asyncHandler(async (req, res) => {
     
     const {fullname,email,username,password} = req.body
@@ -52,5 +55,75 @@ const registerUser = asyncHandler(async (req, res) => {
 }
 );
 
+const loginUser = asyncHandler(async (req, res) => {
+    const {username, password} = req.body
+    if (!username || !password) {
+        throw new Apierror('Please fill all fields', 400)
+    }
 
-export default registerUser;
+    const user = await User.findOne({username})
+
+    if (!user) {
+        throw new Apierror('Register Yourself', 401)
+    }
+
+    const ispasswordvalid = await user.isPasswordCorrect(password)
+    if (!ispasswordvalid) {
+        throw new Apierror('Invalid credentials', 401)
+    }
+
+    const {refresh, access} = await generateAccessAndRefreshToken(user,User)
+
+    //Now the user is authenticated
+    const loggedInUser = await User.findById(user.id).select('-password -refreshToken')
+    const option = {
+        httpOnly: true,
+        secure: true,
+    }
+    return res.status(200).cookie('refresh', refresh, option).cookie('access',access,option).json(
+        new APiResponse(200, {access, refresh, user: loggedInUser}, 'User logged in successfully'))
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user.id,
+        {$set :{
+            refreshToken : ""
+        }}
+    )
+    option = {
+        httpOnly: true,
+        secure: true,
+    }
+    return res.status(200).clearCookie('refresh', option).clearCookie('access', option).json(
+        new APiResponse(200, {}, 'User logged out successfully')
+    )    
+})
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+
+    const token = req.cookies?.refresh || req.headers['x-refresh-token'] || req.headers('authorization')?.replace('Bearer ', '');
+    if (!token) {
+        throw new Apierror('Please login to access this route', 401)
+    }
+    const decodetoken = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
+    const user = await User.findById(decodetoken._id)
+    if (!user) {
+        throw new Apierror('User not found', 404)
+    }
+    if (user.refreshToken !== token) {
+        throw new Apierror('Invalid token', 401)
+    }
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    const {Nrefresh, access} = await generateAccessAndRefreshToken(user,User)
+    return res.status(200).cookie('refresh', Nrefresh, options).cookie('access', access, options).json(
+        new APiResponse(200, {access, Nrefresh}, 'Token refreshed successfully')
+    )
+
+})
+
+export  {registerUser,loginUser,logoutUser,refreshAccessToken};
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,6 +1,9 @@
 import {Router} from 'express';
 import registerUser from '../controllers/user.controller.js';
 import {upload} from '../middlewares/multer.middleware.js';
+import { loginUser, logoutUser, refreshAccessToken } from '../controllers/user.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
+
 
 
 const router = Router();
@@ -20,4 +23,7 @@ router.route('/register').post(
     ),
     registerUser)
 
+router.route('/login').post(loginUser)
+router.route('/logout').get(verifyJWT,logoutUser)
+router.route('/refresh').post(refreshAccessToken)
 export default router;
